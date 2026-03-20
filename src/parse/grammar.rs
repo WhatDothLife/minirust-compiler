d9b24e9521305e377c3b82d298ff2d1b57a1fcbe
Michael Wernthaler <michael.wernthaler@posteo.de>
2026-03-20 18:20:56 +0100

    Enforce semicolon in let statements

--- src/parse/grammar.rs
+++ src/parse/grammar.rs
@@ -59,29 +59,8 @@ peg::parser! {
 
         // Atomic terms 
         #[cache_left_rec]
-        rule _term() -> ast::Term
-            // let: 4 variants (continuation yes/no, type annotation yes/no).
-            // Missing continuation defaults to Unit.
-            = "let" spa() __ i:ident() __ "=" __ t:term() _ "\n" __ c:term()
-              { ast::Term::Let(i, None, t, c) }
-            / "let" spa() __ s:position!() i:ident() e:position!() __ "=" __ t:term()
-              { ast::Term::Let(i, None, t, Tag::new(ast::Term::Unit, (s, e))) }
-            / "let" spa() __ i:ident() _ ":" _ ty:typ() __  "=" __ t:term() _ "\n" __ c:term()
-              { ast::Term::Let(i, Some(ty), t, c) }
-            / "let" spa() __ s:position!() i:ident() e:position!()  _ ":" _ ty:typ() __ "=" __ t:term()
-              { ast::Term::Let(i, Some(ty), t, Tag::new(ast::Term::Unit, (s, e))) }
-            // fn: 4 variants (continuation yes/no, return type yes/no).
-            // Missing return type defaults to Unit.
-            // Missing continuation defaults to Unit.
-            / "fn" spa() i:ident() _ "(" _ ts:list(<tuple_colon(<ident()>, <typ()>)>) _ ")" _ "->" _ ty:typ() __ "{" __ t:term() __ "}" _ "\n" __ c:term()
-              { ast::Term::FunDec(i, ts, ty, t, c) }
-            / "fn" spa() i:ident() _ "(" _ ts:list(<tuple_colon(<ident()>, <typ()>)>) _ ")" _ "->" _ ty:typ() __ "{" __ t:term() __ "}"
-              { ast::Term::FunDec(i.clone(), ts, ty, t, Tag::new(ast::Term::Unit, i.span)) }
-            / "fn" spa() i:ident() _ "(" _ ts:list(<tuple_colon(<ident()>, <typ()>)>) _ ")" _ "{" __ t:term() __ "}" _ "\n" __ c:term()
-              { ast::Term::FunDec(i.clone(), ts, Tag::new(ast::Type::Unit, i.span), t, c) }
-            / "fn" spa() i:ident() _ "(" _ ts:list(<tuple_colon(<ident()>, <typ()>)>) _ ")" _ "{" __ t:term() __ "}"
-              { ast::Term::FunDec(i.clone(), ts, Tag::new(ast::Type::Unit, i.span), t, Tag::new(ast::Term::Unit, i.span)) }
-            / i:i64() { ast::Term::Int(i) }
+        rule atom() -> ast::Term
+            = i:i64() { ast::Term::Int(i) }
             / "()" { ast::Term::Unit }
             / "(" __ t:term() __ ")" { t.into_inner() }
             / i:ident() { ast::Term::Var(i)   }
@@ -90,9 +69,6 @@ peg::parser! {
         #[cache_left_rec]
         pub rule term() -> ast::_Term = precedence! {
             s:position!() t:@ e:position!() { Tag::new(t, (s, e)) }
-            --
-            l:(@) _ ";" __ r:@ { ast::Term::Seq(l, r) }
-            l:(@) _ s:position!() ";" __ e:position!() { ast::Term::Seq(l.clone(), Tag::new(ast::Term::Unit, l.span)) }
             --
             x:(@) __ s:position!() "==" e:position!() __ y:@ { ast::Term::BinOp(x, Tag::new(ast::BinOp::Eq, (s, e)), y) }
             x:(@) __ s:position!() "!=" e:position!() __ y:@ { ast::Term::BinOp(x, Tag::new(ast::BinOp::Neq, (s, e)), y) }
@@ -110,17 +86,51 @@ peg::parser! {
             --
             l:(@) _ "(" __ ts:list(<term()>) __ ")" { ast::Term::FunApp(l, ts) }
             --
-            t:_term() { t }
+            t:atom() { t }
         }
+
+        #[cache_left_rec]
+        pub rule seq() -> ast::_Term = precedence! {
+            s:position!() t:@ e:position!() { Tag::new(t, (s, e)) }
+            --
+            // let: 4 variants (type annotation yes/no, continuation yes/no).
+            // Missing continuation defaults to Unit.
+             "let" spa() __ i:ident() __ "=" __ t:term() _ ";" __  c:seq()
+              { ast::Term::Let(i, None, t, c) }
+             "let" spa() __ s:position!() i:ident() e:position!() __ "=" __ t:term() _ ";" __
+              { ast::Term::Let(i, None, t, Tag::new(ast::Term::Unit, (s, e))) }
+             "let" spa() __ i:ident() _ ":" _ ty:typ() __  "=" __ t:term() _ ";" __  c:seq()
+              { ast::Term::Let(i, Some(ty), t, c) }
+             "let" spa() __ s:position!() i:ident() e:position!()  _ ":" _ ty:typ() __ "=" __ t:term() _ ";" __
+              { ast::Term::Let(i, Some(ty), t, Tag::new(ast::Term::Unit, (s, e))) }
+            // fn: 4 variants (continuation yes/no, return type yes/no).
+            // Missing return type defaults to Unit.
+            // Missing continuation defaults to Unit.
+             "fn" spa() i:ident() _ "(" _ ts:list(<tuple_colon(<ident()>, <typ()>)>) _ ")" _ "->" _ ty:typ() __ "{" __ t:seq() __ "}" _ "\n" __ c:seq()
+              { ast::Term::FunDec(i, ts, ty, t, c) }
+             "fn" spa() i:ident() _ "(" _ ts:list(<tuple_colon(<ident()>, <typ()>)>) _ ")" _ "->" _ ty:typ() __ "{" __ t:seq() __ "}"
+              { ast::Term::FunDec(i.clone(), ts, ty, t, Tag::new(ast::Term::Unit, i.span)) }
+             "fn" spa() i:ident() _ "(" _ ts:list(<tuple_colon(<ident()>, <typ()>)>) _ ")" _ "{" __ t:seq() __ "}" _ "\n" __ c:seq()
+              { ast::Term::FunDec(i.clone(), ts, Tag::new(ast::Type::Unit, i.span), t, c) }
+             "fn" spa() i:ident() _ "(" _ ts:list(<tuple_colon(<ident()>, <typ()>)>) _ ")" _ "{" __ t:seq() __ "}"
+              { ast::Term::FunDec(i.clone(), ts, Tag::new(ast::Type::Unit, i.span), t, Tag::new(ast::Term::Unit, i.span)) }
+            
+            --
+            l:(@) _ ";" __ r:@ { ast::Term::Seq(l, r) }
+            l:(@) _ s:position!() ";" __ e:position!() { ast::Term::Seq(l.clone(), Tag::new(ast::Term::Unit, l.span)) }
+            --
+            e:term() { e.into_inner() }
+        }
+
 
         // Program
         rule top() -> ast::_Top = precedence! {
             s:position!() t:@ e:position!() { Tag::new(t, (s, e)) }
             --
-            "fn" spa() i:ident() _ "(" __ ts:list(<tuple_colon(<ident()>, <typ()>)>) __ ")" _ "->" _ ty:typ() __ "{" __ t:term() __ "}"
+            "fn" spa() i:ident() _ "(" __ ts:list(<tuple_colon(<ident()>, <typ()>)>) __ ")" _ "->" _ ty:typ() __ "{" __ t:seq() __ "}"
             { ast::Top::FunDec(i, ts, ty, t) }
 
-            "fn" spa() i:ident() _ "(" __ ts:list(<tuple_colon(<ident()>, <typ()>)>) __ ")" _ "{" __ t:term() __ "}"
+            "fn" spa() i:ident() _ "(" __ ts:list(<tuple_colon(<ident()>, <typ()>)>) __ ")" _ "{" __ t:seq() __ "}"
             { ast::Top::FunDec(i.clone(), ts, Tag::new(ast::Type::Unit, i.span), t) }
         }
 
